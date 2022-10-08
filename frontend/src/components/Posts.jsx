9212a6b8c7753f1b62d476dfb3ddd5f78ac5fa83
nuel07 <yehanganep@gmail.com>
2022-10-08 20:12:19 +0300

    fix 1

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,7 +1,7 @@
 import './posts.css'
 import Post from './Post'
 
-export default function Posts({posts}) {
+export default function Posts({ posts }) {
   return (
     <div className='posts'>
       {posts.map(p=>(
