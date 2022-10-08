4c6ccac7f2c503cc2b1ed0c0639c21c0ddb7fa9b
nuel07 <yehanganep@gmail.com>
2022-10-08 20:17:49 +0300

    version 2 posts fix

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,10 +1,13 @@
 import './posts.css'
 import Post from './Post'
+import { useState } from 'react'
 
-export default function Posts({ posts }) {
+
+export default function Posts() {
+  const [posts] = useState([])
   return (
     <div className='posts'>
-      {posts.map(p=>(
+      {posts.map((p)=>(
         <Post post={p}/>
       ))}
     </div>
