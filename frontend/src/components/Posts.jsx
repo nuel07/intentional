2d7010acdc71ed632f74e5a08a7248179d997446
nuel07 <yehanganep@gmail.com>
2022-10-09 13:36:08 +0300

    posts fix

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -2,9 +2,24 @@ import './posts.css'
 import Post from './Post'
 import { useContext } from 'react'
 import { Context } from '../context/postsContext'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 
-export default function Posts({posts}) {
+export default function Posts() {
+  const [posts, setPosts] = useState();
+  const getPosts = async () => {
+    try {
+      const res = await axios.get("/posts")
+      const data = await res.data
+      return data
+    } catch (error) {
+      res.status(500).json(error)
+    }
+    useEffect(() => {
+      getPosts().then(data => setPosts(data.posts))
+    }, [])
+  }
   return (
     <>
       <div className='posts'>
