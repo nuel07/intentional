6df9849ad487e755012d940234f3e2b1be254625
nuel07 <yehanganep@gmail.com>
2022-10-09 13:57:59 +0300

    fix

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,29 +1,17 @@
 import './posts.css'
 import Post from './Post'
 import { useContext } from 'react'
-import { Context } from '../context/postsContext'
-import { useEffect } from 'react'
 import { useState } from 'react'
-import axios from 'axios'
-
+//import { Context } from '../context/postsContext'
 
 export default function Posts() {
-  const [posts, setPosts] = useState();
-  const getPosts = async () => {
-    try {
-      const res = await axios.get("/posts")
-      const data = await res.data
-      return data
-    } catch (error) {
-    }
-  }
-  useEffect(() => {
-    getPosts().then(data => setPosts(data.posts))
-  }, [])
+  const [posts] = useState()
   return (
     <>
       <div className='posts'>
-        <Post />
+      {posts.map((p)=>(
+        <Post post={p}/>
+      ))}
     </div>
     </>
   )
